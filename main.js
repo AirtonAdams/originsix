31e4ea25a8fd08f2d8da7c7a0463721bfbfbcf89
Airton Adams <68087823+AirtonAdams@users.noreply.github.com>
2022-08-15 11:20:51 -0300

    Fix nav buttons and hover effects

--- main.js
+++ main.js
@@ -2,6 +2,31 @@ window.addEventListener('scroll', onScroll)
 onScroll()
 function onScroll() {
   showNavOnScroll()
+  activateManuAtCurrentSection(home)
+  activateManuAtCurrentSection(about)
+  activateManuAtCurrentSection(services)
+  activateManuAtCurrentSection(depositions)
+  activateManuAtCurrentSection(contact)
+}
+function activateManuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetline
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 function showNavOnScroll() {
   const nav = document.getElementById('navigation')
